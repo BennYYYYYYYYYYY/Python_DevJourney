132d65d1e9a7c4761c7b0dfe7321e06612657c68
HUANG HSIANG <129297292+BennYYYYYYYYYYY@users.noreply.github.com>
2024-10-17 13:48:47 +0800

    Delete 6.Advanced_Functions directory

--- 6.Advanced_Functions/5.Stdin_Stdout_Pipe.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def main():
-    contents = sys.stdin.read() # standard input
-    new_contents = contents.replace(sys.argv[1], sys.argv[2]) 
-    sys.stdout.write(new_contents) # standard output
-
-main()
