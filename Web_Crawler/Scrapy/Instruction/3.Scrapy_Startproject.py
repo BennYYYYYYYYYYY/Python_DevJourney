51297c4f61c716800ca78ce9e447a51c828c6223
HUANG HSIANG <129297292+BennYYYYYYYYYYY@users.noreply.github.com>
2024-11-28 15:51:04 +0800

    Add files via upload

---
+++ Web_Crawler/Scrapy/Instruction/3.Scrapy_Startproject.py
@@ -0,0 +1,20 @@
+# conda activate scrapyenv
+# pip install scrapy
+# scrapy startproject ScrapyReddit
+# cd ScrapyReddit (C:\Users\user\Desktop\Scrapy-Reddit\ScrapyReddit>)
+# scrapy genspider ${spider_name} ${target_domain_address} (建立一個新的爬蟲檔案： 新增一個 .py 檔在 spiders 資料夾中就完成了。)
+'''
+├── ScrapyReddit
+│   ├── __init__.py
+│   ├── items.py
+│   ├── middlewares.py
+│   ├── pipelines.py
+│   ├── settings.py
+│   └── spiders
+│       ├── __init__.py
+│       └── example.py
+└── scrapy.cfg
+'''
+
+
+
